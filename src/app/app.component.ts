ffa0f59cc6873d1a3269478b528bc83493797860
Stavros <stavros@thortful.com>
2021-11-09 22:32:26 +0000

    add library to translate csv to array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { TransactionService } from './transaction.service';
+import { TransactionService } from './services/transaction.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'graph-project';
   constructor(private readonly transactions: TransactionService){
-    this.transactions.getTransaction().subscribe((res)=>console.log(res))
+    this.transactions.getTransaction().subscribe((res)=>{
+    console.log(res)
+    })
   }
 }
---
+++ src/app/services/transaction.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Papa, ParseResult } from 'ngx-papaparse';
+import { switchMap, map } from 'rxjs/operators';
+
+export enum TransactionMonth {
+  DEC = 'Dec2016.csv',
+  NOV = 'Nov2016.csv',
+  OCT = 'Oct2016.csv',
+  SEP = 'Sep2016.csv',
+  AUG = 'Aug2016.csv',
+  JUL = 'Jul2016.csv',
+  JUN = 'Jun2016.csv',
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  constructor(private readonly http: HttpClient, private papa: Papa) {}
+  getTransaction() {
+    return this.http
+      .get(`assets/${TransactionMonth.DEC}`, { responseType: 'text' })
+      .pipe(
+        switchMap((res) => {
+          return new Promise<ParseResult>((resolve) => {
+            this.papa.parse(res, {
+              complete: (result:ParseResult) => {
+                resolve(result);
+              },
+            });
+          });
+        })
+        ,map((res:ParseResult):string[]=>{
+          return res.data.slice(4)
+        })
+      );
+  }
+}
