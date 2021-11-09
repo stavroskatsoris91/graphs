8f838326790950663e1db78c890b3782d90248a3
Stavros <stavros@thortful.com>
2021-11-09 00:18:20 +0000

    call transaction csv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransactionService } from './transaction.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graph-project';
+  constructor(private readonly transactions: TransactionService){
+    this.transactions.getTransaction().subscribe((res)=>console.log(res))
+  }
 }
